57ea2ea53ab00987b0c9c0b2b993b29059665dfd
Vladyslav Krasovsky <v.o.krasovsky@gmail.com>
2016-07-31 23:59:07 +0300

    Config/Assets: Added Cache Busting

--- config/web.php
+++ config/web.php
@@ -86,6 +86,9 @@ $config = [
             'siteKey' => '6LfvziUTAAAAAPAv58rNBdRI1e6iVmZbSKTVFUHV',
             'secret' => '6LfvziUTAAAAAPcBTGyDIm8vxT_PLKIEOw0kBplS',
         ],
+        'assetManager' => [
+            'appendTimestamp' => true,
+        ],
     ],
     'params' => $params,
 ];
