9fe1c48ea0dbb7381039f7700bc1a77753468a8d
Vladyslav Krasovsky <v.o.krasovsky@gmail.com>
2016-07-31 16:11:28 +0300

    MenuWidget: hide category when visible is Off

--- components/menu_tpl/menu.php
+++ components/menu_tpl/menu.php
@@ -1,10 +1,17 @@
-<?php if( isset($category['children']) ): ?>
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= $category['title'] ?><span class="caret"></span></a>
-        <ul class="dropdown-menu">
-            <?= $this->getMenuHtml($category['children']); ?>
-        </ul>
-    </li>
-<?php else: ?>
-    <li><a href="<?= \yii\helpers\Url::to(['category/view', 'alias' => $category['alias']]) ?>"><?= $category['title'] ?></a></li>
+<?php
+use app\models\Category;
+?>
+<?php if( $category['visible'] == Category::VISIBLE_ON ): ?>
+
+    <?php if( isset($category['children']) ): ?>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= $category['title'] ?><span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <?= $this->getMenuHtml($category['children']); ?>
+            </ul>
+        </li>
+    <?php else: ?>
+        <li><a href="<?= \yii\helpers\Url::to(['category/view', 'alias' => $category['alias']]) ?>"><?= $category['title'] ?></a></li>
+    <?php endif; ?>
+
 <?php endif; ?>
